fb4aeef38949753c21e35497306b00cc00e419a8
differui <differui@gmail.com>
2017-05-27 15:58:22 +0800

    Use rollup eslint plugin

--- generators/app/templates/rollup.config.js
+++ generators/app/templates/rollup.config.js
@@ -1,4 +1,5 @@
 const commonjs = require('rollup-plugin-commonjs')
+const eslint = require('rollup-plugin-eslint')
 const replace = require('rollup-plugin-replace')
 const resolve = require('rollup-plugin-node-resolve')<% if (buble) { %>
 const buble = require('rollup-plugin-buble')<% } %><% if (babel) { %>
@@ -9,6 +10,7 @@ export default {
   dest: './dest/bundle.js',
   format: 'cjs',
   plugins: [<% if (buble) { %>
+    eslint(),
     buble({
       exclude: 'node_modules/**'
     }),<% } %><% if (babel) { %>
